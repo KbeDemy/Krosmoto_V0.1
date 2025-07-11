956f2dd7f9b48b07fb12bb00d439a57f76e596be
Kobe <87835153+KbeDemy@users.noreply.github.com>
2025-07-12 00:59:08 +0200

    Update server.py

--- server/server.py
+++ server/server.py
@@ -21,20 +21,4 @@ if __name__ == '__main__':
 from flask import Flask, render_template
 import sqlite3
 
-app = Flask(__name__)
 
-def get_data():
-    conn = sqlite3.connect('sensor_data.db')  # Zelfde als logger!
-    cursor = conn.cursor()
-    cursor.execute("SELECT rowid, value, timestamp FROM sensor_data ORDER BY timestamp DESC LIMIT 20")
-    data = cursor.fetchall()
-    conn.close()
-    return data
-
-@app.route('/')
-def index():
-    data = get_data()
-    return render_template('index.html', data=data)
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
